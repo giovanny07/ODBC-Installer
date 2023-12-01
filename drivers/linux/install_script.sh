381f8bbf621ed0274ec5c8c4079ddfd895e153fa
giovanny07 <giovanny.rodriguez@imagunet.com>
2023-11-30 19:58:13 -0500

    Add function to install unixODBC

--- drivers/linux/install_script.sh
+++ drivers/linux/install_script.sh
@@ -34,11 +34,46 @@ detect_linux_distribution() {
     fi
 }
 
+# Function to update repositories
+update_repositories() {
+    if command -v apt &> /dev/null; then
+        sudo apt-get update
+    elif command -v yum &> /dev/null; then
+        sudo yum check-update
+    else
+        echo "Unsupported package manager. Please add the necessary checks for your package manager."
+    fi
+}
+
+# Function to install a package if not present
+install_package_if_not_present() {
+    PACKAGE_NAME=$1
+    if ! command -v $PACKAGE_NAME &> /dev/null; then
+        echo "$PACKAGE_NAME not found. Installing..."
+        if command -v apt &> /dev/null; then
+            sudo apt-get install $PACKAGE_NAME
+        elif command -v yum &> /dev/null; then
+            sudo yum install $PACKAGE_NAME
+        else
+            echo "Unsupported package manager. Please add the necessary checks for your package manager."
+            exit 1
+        fi
+    else
+        echo "$PACKAGE_NAME is already installed."
+    fi
+}
+
+# Function to check and update repositories, and install unixODBC if not present
+check_and_install_unixodbc() {
+    update_repositories
+    install_package_if_not_present "unixODBC"
+}
+
 # Call the function to check internet connection
 check_internet_connection
 
-# Call the function to detect Linux distribution
-detect_linux_distribution
+# Call the function to check and install unixODBC
+check_and_install_unixodbc
 
 
 
